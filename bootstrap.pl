134bb9f39c53fc0e54546bfee5a2af033d1366a9
J M Maurer <uwog@uwog.net>
2006-06-17 14:10:32 +0000

    - Add support to apply patches to the tree - Add a patch to force layouting when doing command line conversions;   this greatly increases the scope of the code tested.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword-testsuite/trunk@25388 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- bootstrap.pl
+++ bootstrap.pl
@@ -39,6 +39,12 @@ if ($branch eq "ABI-2-4-0-STABLE")
 	 cvs -d $cvsroot -z3 co -r $branch abi abidistfiles abiword-plugins &&\
 	 cvs -d $cvsroot -z3 co -r wv-1-0-0-STABLE wv`;
 
+	# apply the testsuite specific patches to the tree
+	foreach $patch ( @patches )
+	{
+		`cd $source_dir && patch -p0 < $root/patches/$patch`;
+	}
+
 	# build abiword
 	# TODO: write the compilation report to a branch specific log file
 	`cd $source_dir/abi &&\
@@ -58,6 +64,12 @@ elsif ($branch eq "HEAD")
 	`cd $source_dir &&\
 	 cvs -d $cvsroot -z3 co -r $branch abi abidistfiles abiword-plugins &&\
 	 cvs -d $cvsroot -z3 co wv`;
+
+	# apply the testsuite specific patches to the tree
+	foreach $patch ( @patches )
+	{
+		`cd $source_dir && patch -p0 < $root/patches/$patch`;
+	}
 
 	# build wv
 	`cd $source_dir/wv &&\
