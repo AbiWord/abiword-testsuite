4b9e2b190ce9104bc2cc898f6d0eec11117593a3
J M Maurer <uwog@uwog.net>
2006-08-03 22:14:36 +0000

    add ompl to the import list (even through STABLE won't be able to handle it)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword-testsuite/trunk@25426 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- impexp/regenerate_raw.pl
+++ impexp/regenerate_raw.pl
@@ -5,7 +5,7 @@ sub GenRaw {
 	my ($branch) = @_;
 
 	# TODO: Move this to regression.conf
-	$importDirs = 'abw rtf txt odt doc wpd xml';
+	$importDirs = 'abw rtf txt odt doc wpd xml ompl';
 
 	@subDirList = split(/\s+/, $importDirs);
 
